cd607e3f71a16a5352f6897dc4ff0168fb9d8cb4
prashantc29 <prash.c.29@gmail.com>
2014-12-08 19:48:27 -0600

    Adding caching for TFIDF and LDA models #7

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 import numpy as np
 import scipy
+import scipy.io
 import sys
 import math
 from random import sample
 import time
 import pandas as p
-from sklearn import metrics,preprocessing,cross_validation
+from sklearn import metrics, preprocessing, cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 import sklearn.linear_model as lm
 from sklearn.naive_bayes import GaussianNB
@@ -20,98 +20,139 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 debug = 0
 
+# LDA parameters
+numTopics = 1000
+numIterations = 10
+
+
 def convert(val):
   if val == '?':
     return 0
-  elif isinstance(val,basestring):
+  elif isinstance(val, basestring):
     return float(val)
   else:
     return val
 
+
 def isImbalanced(y):
-  class0=0.0
-  class1=0.0
+  class0 = 0.0
+  class1 = 0.0
   for e in y:
     if e == 0:
-      class0+=1
+      class0 += 1
     else:
-      class1+=1
+      class1 += 1
   if debug == 1:
     print 'Class 0:', class0, 'Class 1:', class1
-  if (max(class0,class1)/min(class0,class0) > 3):
+  if (max(class0, class1) / min(class0, class0) > 3):
     print 'Imbalanced Classes'
   else:
     print 'No Imbalanced Classes'
 
-def tfidf(trainData):
-  tfv = TfidfVectorizer(min_df=3,  max_features=None, strip_accents='unicode',  
-        analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), use_idf=1,smooth_idf=1,sublinear_tf=1)
-  tfv.fit(trainData)
-  X = tfv.transform(trainData)
+
+def tfidf(data, isTrain):
+  if isTrain == True:
+    filename = 'cache/tfidf_train'
+  else:
+    filename = 'cache/tfidf_test'
+  try:
+    X = scipy.io.mmread(filename + '.mtx').tocsr()
+    print "Loaded tfidf model from", filename
+  except IOError:
+    print filename + " not present. Building TFIDF again."
+    tfv = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode',
+                          analyzer='word', token_pattern=r'\w{1,}', ngram_range=(1, 2), use_idf=1, smooth_idf=1,
+                          sublinear_tf=1, stop_words='english')
+    tfv.fit(data)
+    X = tfv.transform(data)
+    scipy.io.mmwrite(filename, X)
   return X
+
 
 def documentFrequencyVector(trainData):
-  tfv = CountVectorizer(min_df=3,  max_features=None, strip_accents='unicode',  
-        analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), stop_words='english')
+  tfv = CountVectorizer(min_df=3, max_features=None, strip_accents='unicode',
+                        analyzer='word', token_pattern=r'\w{1,}', ngram_range=(1, 2), stop_words='english')
   tfv.fit(trainData)
   X = tfv.transform(trainData)
   return X
 
+
 def getLdaTopicsVector(X):
-  model = lda.LDA(n_topics=20, n_iter=1, random_state=1)
-  model.fit(X)  # model.fit_transform(X) is also available
-  doc_topic = model.doc_topic_
+  doc_topic = transformInLDATopics(X)
   topicVector = []
   for i in range(X.shape[0]):
     topicVector.append(doc_topic[i].argmax())
   return topicVector
 
+
 def transformInLDATopics(X):
-  model = lda.LDA(n_topics=1000, n_iter=10, random_state=1)
-  model.fit(X)  # model.fit_transform(X) is also available
-  return model.doc_topic_
+  filename = 'cache/doc_topic_' + str(numTopics) + '_iterations_' + str(numIterations)
+  try:
+    doc_topic = np.load(filename + '.npy')
+    print "Loaded doc_topic from", filename
+    print "doc_topic size", doc_topic.shape
+    print "doc_topic type", type(doc_topic)
+  except IOError:
+    print filename + " not present. Building LDA again."
+    X = documentFrequencyVector(X)
+    model = lda.LDA(n_topics=numTopics, n_iter=numIterations, random_state=1)
+    model.fit(X)  # model.fit_transform(X) is also available
+    doc_topic = model.doc_topic_
+    np.save(filename, doc_topic)
+  return doc_topic
+
 
 def selectModel(classifier):
   if classifier == 0:
     print 'Model: Logistic Regression'
-    model = lm.LogisticRegression(penalty='l2', dual=True, tol=0.0001, C=1, fit_intercept=True, intercept_scaling=1.0, class_weight=None, random_state=None)
+    model = lm.LogisticRegression(penalty='l2', dual=True, tol=0.0001, C=1, fit_intercept=True,
+                                  intercept_scaling=1.0, class_weight=None, random_state=None)
   elif classifier == 1:
     print 'Model: Naive Bayes'
     model = MultinomialNB(alpha=1)
   elif classifier == 2:
     print 'Model: Random Forest'
-    #model = RandomForestClassifier(n_estimators=10, max_features="auto", max_depth=2, bootstrap=True, n_jobs=4)
-    model = RandomForestClassifier(n_estimators=10, max_features="auto", max_depth=None, bootstrap=True, n_jobs=4, criterion="entropy")
-    #model = RandomForestClassifier(n_estimators=200, max_features=None, max_depth=None, bootstrap=True, n_jobs=-1, verbose=1)
+    # model = RandomForestClassifier(n_estimators=10, max_features="auto", max_depth=2, bootstrap=True, n_jobs=4)
+    model = RandomForestClassifier(n_estimators=10, max_features="auto", max_depth=None, bootstrap=True, n_jobs=4,
+                                   criterion="entropy")
+    # model = RandomForestClassifier(n_estimators=200, max_features=None, max_depth=None, bootstrap=True, n_jobs=-1, verbose=1)
     #model = RandomForestClassifier(n_estimators=10)
+  else:
+    print 'Invalid model: ', classifier
+    sys.exit(-1)
   return model
 
+
 def featureEng(trainFile):
-  trainData = np.array(p.read_table(trainFile, converters={'alchemy_category_score':convert, 'is_news':convert, 'news_front_page':convert}))
+  trainData = np.array(p.read_table(trainFile, converters={'alchemy_category_score': convert, 'is_news': convert,
+                                                           'news_front_page': convert}))
   # Delete unnecessary features
   # Delete URL, URLID, BolierPlate, Alchemy_Category, Label
-  #trainData = np.delete(trainData, [0,1,2,3,11,12,14,-1], 1)
-  trainData = np.delete(trainData, [0,1,2,3,11,12,14,21,26], 1)
+  # trainData = np.delete(trainData, [0,1,2,3,11,12,14,-1], 1)
+  trainData = np.delete(trainData, [0, 1, 2, 3, 11, 12, 14, 21, 26], 1)
 
   # This fixed the attribute error(while predicting)
   trainData = np.array(trainData, dtype=np.float)
   return trainData
 
+
 def featureEngTest(testFile):
-  testData = np.array(p.read_table(testFile, converters={'alchemy_category_score':convert, 'is_news':convert, 'news_front_page':convert}))
+  testData = np.array(p.read_table(testFile, converters={'alchemy_category_score': convert, 'is_news': convert,
+                                                         'news_front_page': convert}))
   # Delete unnecessary features
   # Delete URL, URLID, BolierPlate, Alchemy_Category, Label
-  #trainData = np.delete(trainData, [0,1,2,3,11,12,14,-1], 1)
-  testData = np.delete(testData, [0,1,2,3,11,12,14,21], 1)
+  # trainData = np.delete(trainData, [0,1,2,3,11,12,14,-1], 1)
+  testData = np.delete(testData, [0, 1, 2, 3, 11, 12, 14, 21], 1)
 
   # This fixed the attribute error(while predicting)
   testData = np.array(testData, dtype=np.float)
   return testData
 
+
 def plotFeatureImportances(forest):
   importances = forest.feature_importances_
   std = np.std([tree.feature_importances_ for tree in forest.estimators_],
-             axis=0)
+               axis=0)
   indices = np.argsort(importances)[::-1]
 
   # Print the feature ranking
@@ -128,44 +169,43 @@ def plotFeatureImportances(forest):
   plt.xlim([-1, 10])
   plt.show()
 
+
 def egon(rawData, trainFile, testFile, classifier, boilerplateOnly):
   # Feature Engineering and Formatting Data
   trainData = featureEng(trainFile)
   if debug == 1:
     print "trainData shape:", trainData.shape
   # For Submission
-  #testData = featureEngTest(testFile)
+  # testData = featureEngTest(testFile)
 
   # TFIDF with LR on boilerplate
-  trainDataBoilerplate = list(np.array(p.read_table('train.tsv'))[:,2])
-  testDataBoilerplate = list(np.array(p.read_table('test.tsv'))[:,2])
-  
-  y = list(np.array(p.read_table('train.tsv'))[:,-1])
-  target = np.array(p.read_table('train.tsv'))[:,-1]
+  trainDataBoilerplate = list(np.array(p.read_table('train.tsv'))[:, 2])
+  testDataBoilerplate = list(np.array(p.read_table('test.tsv'))[:, 2])
+
+  y = list(np.array(p.read_table('train.tsv'))[:, -1])
+  target = np.array(p.read_table('train.tsv'))[:, -1]
   isImbalanced(y)
+
+  model = selectModel(classifier)
 
   if boilerplateOnly == 0:
     print "Feature selection type: TFIDF on boiler plate"
-    X = tfidf(trainDataBoilerplate)
+    X = tfidf(trainDataBoilerplate, True)
   elif boilerplateOnly == 1:
     print "Feature selection type: Non boiler plate features"
     X = trainData
   elif boilerplateOnly == 2:
     print "Feature selection type: Non boiler plate features + topics extracted using LDA"
-    X = documentFrequencyVector(trainDataBoilerplate)
-    topicsVector = getLdaTopicsVector(X)
+    topicsVector = getLdaTopicsVector(trainDataBoilerplate)
     t = np.array(topicsVector)[np.newaxis].T
     trainData = np.append(trainData, t, 1)
     X = trainData
   elif boilerplateOnly == 3:
     print "Feature selection type: topics extracted using LDA"
-    X = documentFrequencyVector(trainDataBoilerplate)
-    X = transformInLDATopics(X)
+    X = transformInLDATopics(trainDataBoilerplate)
   else:
     print "boilerplateOnly value should either be 0, 1, 2 or 3"
-    sys.exit(0)
-
-  model = selectModel(classifier)
+    sys.exit(-1)
 
   # Feature importances
 
@@ -173,18 +213,18 @@ def egon(rawData, trainFile, testFile, classifier, boilerplateOnly):
     print "===== Starting random split ====="
   aucAvg = 0.0
   numcross = 10
-  for i in xrange(numcross): 
-    X_train, X_cv, y_train, y_cv = cross_validation.train_test_split(X, y, test_size = 0.2)
+  for i in xrange(numcross):
+    X_train, X_cv, y_train, y_cv = cross_validation.train_test_split(X, y, test_size=0.2)
     model.fit(X_train, y_train)
-    preds = model.predict_proba(X_cv)[:,1]
+    preds = model.predict_proba(X_cv)[:, 1]
     auc = metrics.roc_auc_score(y_cv, preds)
     if debug == 1:
-      print "NumCross:",i," Auc:",auc
+      print "NumCross:", i, " Auc:", auc
     aucAvg += auc
-  aucAvg = aucAvg/numcross
-  print numcross,"Random split AUC : %f" % aucAvg
- 
-  #plotFeatureImportances(model)
+  aucAvg = aucAvg / numcross
+  print numcross, "Random split AUC : %f" % aucAvg
+
+  # plotFeatureImportances(model)
 
   if debug == 1:
     print "===== Starting k fold validation ====="
@@ -195,23 +235,24 @@ def egon(rawData, trainFile, testFile, classifier, boilerplateOnly):
   for traincv, testcv in cv:
     X_train, X_cv, y_train, y_cv = X[traincv], X[testcv], target[traincv], target[testcv]
     model.fit(X_train, y_train)
-    preds = model.predict_proba(X_cv)[:,1]
+    preds = model.predict_proba(X_cv)[:, 1]
     fold = fold + 1
     auc = metrics.roc_auc_score(y_cv, preds)
     if debug == 1:
-      print "Fold:",fold," Auc:",auc
+      print "Fold:", fold, " Auc:", auc
     aucAvg += auc
-  aucAvg = aucAvg/folds
-  print fold,"fold AUC : %f" % aucAvg
-  
+  aucAvg = aucAvg / folds
+  print fold, "fold AUC : %f" % aucAvg
+
   '''
-  # For Submission
-  model.fit(X, y)
-  preds = model.predict_proba(testData)[:,1]
-  testfile = p.read_csv('test.tsv', sep="\t", na_values=['?'], index_col=1)
-  pred_df = p.DataFrame(preds, index=testfile.index, columns=['label'])
-  pred_df.to_csv('benchmark-kewal.csv')
-  '''
+# For Submission
+model.fit(X, y)
+preds = model.predict_proba(testData)[:,1]
+testfile = p.read_csv('test.tsv', sep="\t", na_values=['?'], index_col=1)
+pred_df = p.DataFrame(preds, index=testfile.index, columns=['label'])
+pred_df.to_csv('benchmark-kewal.csv')
+'''
+
 
 def main():
   global debug
@@ -220,6 +261,7 @@ def main():
     sys.exit(2)
   if len(sys.argv) == 7:
     debug = int(sys.argv[6])
-  egon(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])) 
+  egon(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
+
 
 main()
